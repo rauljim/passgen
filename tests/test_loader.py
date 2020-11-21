d19b4a02abafd67435ae3fe7ed5364744150764c
Raul Jimenez <raul@ryoulive.com>
2020-11-21 09:48:48 +0100

    Fix bug in test
    bringing test coverage to 100%.

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -4,6 +4,6 @@ from passgen.loader import get_words_from_file
 
 class Test(TestCase):
     def test_get_words_from_file(self):
+        get_words_from_file('static/eff_large.txt')
         with self.assertRaises(FileNotFoundError):
             get_words_from_file('file_not_found.txt')
-            get_words_from_file('../static/eff_large.txt')
