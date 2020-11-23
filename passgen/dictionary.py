a8348cf70fd17aa5a8b838ed097a42fd1db2b708
Raul Jimenez <raul@ryoulive.com>
2020-11-23 10:45:29 +0100

    Dictionary returns now a generator instead of a list

--- passgen/dictionary.py
+++ passgen/dictionary.py
@@ -2,10 +2,18 @@ DICTIONARIES = {
     'eff-long',  # https://www.eff.org/files/2016/07/18/eff_large_wordlist.txt
 }
 
+
 def get_words_from_dictionary(dictionary):
+    def get_generator():
+        with open(filename) as f:
+            for line in f:
+                yield line.strip()
+
     if dictionary in DICTIONARIES:
         filename = f'dictionaries/{dictionary}.txt'
     else:
         raise KeyError(f'Dictionary does not exist: {dictionary}')
-    with open(filename) as f:
-        return [line.strip() for line in f]
+    # Notice that if 'yield' was here KeyError would be raised
+    # upon a call to next() due to lazy evaluation.
+    # What we want is that the check is done immediately.
+    return get_generator()
--- tests/test_dictionary.py
+++ tests/test_dictionary.py
@@ -3,7 +3,14 @@ from passgen.dictionary import get_words_from_dictionary
 
 
 class Test(TestCase):
-    def test_get_words_from_file(self):
-        get_words_from_dictionary('eff-long')
+    def test_get_words_from_dictionary(self):
+        words = get_words_from_dictionary('eff-long')
+        # check it's an iterator
+        _ = next(words)
+        # it is a generator (not a sequence)
+        with self.assertRaises(TypeError):
+            print(len(words))
+
+    def test_dictionary_not_found(self):
         with self.assertRaises(KeyError):
-            get_words_from_dictionary('dictionary-not-found')
+            _ = get_words_from_dictionary('dictionary-not-found')
